1e751cd0f17be9e95b909a3f9092ed4dcb1e5908
I-Kouta <kouta.baseball0222@gmail.com>
2023-06-11 17:17:03 +0900

    indexブレードの記述

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Book;
 
 class BooksController extends Controller
 {
     public function hello(){
         echo "hello world!<br>";
         echo "コントローラーを使ったルーティングです";
+    }
+
+    public function index(){
+        $books = Book::get();
+        return view("books.index", compact("books"));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('/', function () {
 // });
 
 Route::get("/hello", "BooksController@hello");
+Route::get("/index", "BooksController@index");
