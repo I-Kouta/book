8c8ffdb552c42b37030faecab1e373829e9a6eb7
I-Kouta <kouta.baseball0222@gmail.com>
2023-06-12 22:24:50 +0900

    本の登録の一連の流れの記述

--- app/Book.php
+++ app/Book.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
+    protected $fillable = [ # 書き換え可能
+        "author_id", "title", "price"
+    ];
+
     public function author(){
         return $this->belongsTo("App\Author");
     }
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,4 @@ Route::get("/hello", "BooksController@hello");
 Route::get("/index", "BooksController@index");
 Route::get("/create-form", "BooksController@createForm");
 Route::post("/author/create", "AuthorsController@authorCreate"); # 登録の際に通過
+Route::post("/book/create", "BooksController@bookCreate"); # 画面遷移ナシ
