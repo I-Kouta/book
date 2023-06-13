76efda89f46af4e4ab87adcc4a25d7a9a6227291
I-Kouta <kouta.baseball0222@gmail.com>
2023-06-13 19:10:57 +0900

    ビューテンプレートの使用

--- resources/views/books/createForm.blade.php
+++ resources/views/books/createForm.blade.php
@@ -1,60 +1,41 @@
-<!DOCTYPE html>
-<html>
+@extends("app")
+@section("content")
+@if($errors->any())
+<div class="alert alert-danger">
+  <ul>
+    @foreach($errors->all() as $error)
+    <li>{{ $error }}</li>
+    @endforeach
+  </ul>
+</div>
+@endif
+<div class="container">
+  <h2 class="page-header">著者を登録する</h2>
+  {!! Form::open(['url' => '/author/create']) !!}
+  <div class="form-group">
+    {{ Form::input('text', 'authorName', null, ['required', 'class' => 'form-control', 'placeholder' => '著者名']) }}
+  </div>
+  <button type="submit" class="btn btn-success pull-right">追加</button>
+  {!! Form::close() !!}
+</div>
 
-<head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-
-<body class="container">
-
-    <header>
-        <a href="/index"><h1 class="page-header">Laravelを使ったCRUD機能の実装</h1></a>
-    </header>
-    @if($errors->any())
-    <div class="alert alert-danger">
-      <ul>
-        @foreach($errors->all() as $error)
-        <li>{{ $error }}</li>
+<div class="container">
+  <h2 class="page-header">本を登録する</h2>
+  <div class="form-group">
+    <form action="/book/create" method="post">
+      @csrf
+      <select class="form-select" aria-label="Default select example" name="author_id">
+        <option value="0">著者を選択してください。</option>
+        @foreach($authors as $author)
+        <option value="{{ $author->id }}">
+          {{ $author->name }}
+        </option>
         @endforeach
-      </ul>
-    </div>
-    @endif
-    <div class="container">
-        <h2 class="page-header">著者を登録する</h2>
-        {!! Form::open(['url' => '/author/create']) !!}
-        <div class="form-group">
-            {{ Form::input('text', 'authorName', null, ['required', 'class' => 'form-control', 'placeholder' => '著者名']) }}
-        </div>
-        <button type="submit" class="btn btn-success pull-right">追加</button>
-        {!! Form::close() !!}
-    </div>
-
-    <div class="container">
-      <h2 class="page-header">本を登録する</h2>
-      <div class="form-group">
-        <form action="/book/create" method="post">
-          @csrf
-          <select class="form-select" aria-label="Default select example" name="author_id">
-            <option value="0">著者を選択してください。</option>
-            @foreach($authors as $author)
-            <option value="{{ $author->id }}">
-              {{ $author->name }}
-            </option>
-            @endforeach
-          </select>
-          <input type="text" name="title" value="" class="form-control" placeholder="本のタイトル">
-          <input type="text" name="price" value="" class="form-control" placeholder="本の金額">
-          <button type="submit" class="btn btn-success pull-right">追加</button>
-        </form>
-      </div>
-    </div>
-
-    <footer>
-        <small>Laravel@test.curriculum</small>
-    </footer>
-</body>
-
-</html>
+      </select>
+      <input type="text" name="title" value="" class="form-control" placeholder="本のタイトル">
+      <input type="text" name="price" value="" class="form-control" placeholder="本の金額">
+      <button type="submit" class="btn btn-success pull-right">追加</button>
+    </form>
+  </div>
+</div>
+@endsection
--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,56 +1,37 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-
-<body class="container">
-
-    <header>
-        <a href="/index"><h1>Laravelを使ったCRUD機能の実装</h1></a>
-    </header>
-
-    <div class="container">
-        <form action="/search" method="post">
-            @csrf
-            <input type="text" name="keyword" class="form" placeholder="タイトルで検索">
-            <button type="submit" class="btn btn-success">検索</button>
-            @if(!empty($keyword))
-            <div>検索ワード：{{ $keyword }}</div>
-            @endif
-        </form>
-        <p class="pull-right"><a class="btn btn-success" href="/create-form">登録する</a></p>
-        <h2 class="page-header">本のリスト一覧</h2>
-        <table class='table table-hover'>
-            <tr>
-                <th>No</th>
-                <th>タイトル</th>
-                <th>著者名</th>
-                <th>金額</th>
-                <th>登録日時</th>
-                <th></th>
-                <th></th>
-            </tr>
-            @foreach ($books as $book)
-            <tr>
-                <td>{{ $book->id }}</td>
-                <td>{{ $book->title }}</td>
-                <td>{{ $book->author->name }}</td>
-                <td>{{ $book->price }}</td>
-                <td>{{ $book->created_at }}</td>
-                <td><a class="btn btn-primary" href="/book/{{$book->id}}/update-form">更新</a></td>
-                <td><a class="btn btn-danger" href="/book/{{$book->id}}/delete" onclick="return confirm('こちらの本を削除してもよろしいでしょうか？')">削除</a></td>
-            </tr>
-            @endforeach
-        </table>
-    </div>
-    <footer>
-        <small>Laravel@test.curriculum</small>
-    </footer>
-</body>
-
-</html>
+@extends("app")
+@section("content")
+<div class="container">
+    <form action="/search" method="post">
+        @csrf
+        <input type="text" name="keyword" class="form" placeholder="タイトルで検索">
+        <button type="submit" class="btn btn-success">検索</button>
+        @if(!empty($keyword))
+        <div>検索ワード：{{ $keyword }}</div>
+        @endif
+    </form>
+    <p class="pull-right"><a class="btn btn-success" href="/create-form">登録する</a></p>
+    <h2 class="page-header">本のリスト一覧</h2>
+    <table class='table table-hover'>
+        <tr>
+            <th>No</th>
+            <th>タイトル</th>
+            <th>著者名</th>
+            <th>金額</th>
+            <th>登録日時</th>
+            <th></th>
+            <th></th>
+        </tr>
+        @foreach ($books as $book)
+        <tr>
+            <td>{{ $book->id }}</td>
+            <td>{{ $book->title }}</td>
+            <td>{{ $book->author->name }}</td>
+            <td>{{ $book->price }}</td>
+            <td>{{ $book->created_at }}</td>
+            <td><a class="btn btn-primary" href="/book/{{$book->id}}/update-form">更新</a></td>
+            <td><a class="btn btn-danger" href="/book/{{$book->id}}/delete" onclick="return confirm('こちらの本を削除してもよろしいでしょうか？')">削除</a></td>
+        </tr>
+        @endforeach
+    </table>
+</div>
+@endsection
